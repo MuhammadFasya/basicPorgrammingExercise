66109edc8b8bd7103c8534621cf5071df58591e0
unknown <mhmdfasya15@gmail.com>
2024-03-28 14:57:58 +0700

    Task session 6

---
+++ session6/if_else.py
@@ -0,0 +1,13 @@
+# check if a larger than b 
+# check if b larger than c
+a = 6
+b = 5
+c = 4
+
+#if else check
+if(a > b ):
+    print("a is larger than b and c")
+elif( b > c ):
+    print("b is larger than c")
+else:
+    print("a is not larger than b and c")
