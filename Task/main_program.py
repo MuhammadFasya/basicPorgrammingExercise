b3d26dc19cffc1eb7f084f5650106066a5f761f8
MuhammadFasya <@mhmdfasya15@gmail.com>
2024-06-07 23:09:27 +0700

    task 12

---
+++ Task/main_program.py
@@ -0,0 +1,9 @@
+# main_program.py
+
+import task11_calculator
+
+def main():
+    task11_calculator.main()
+
+if __name__ == "__main__":
+    main()
--- session13/main.py
+++ session13/main.py
@@ -1,4 +1,6 @@
-import studentModule
+#import studentModule
 
-about_fasya = studentModule.getAboutMe("Fasya", "TI23T")
-print(about_fasya)
+#about_fasya = studentModule.getAboutMe("Fasya", "TI23T")
+#print(about_fasya)
+for i in range (3):
+  print(i<3)
